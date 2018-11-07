0811c79b3b6d749f70b2dc612be5b26f3c44d7c4
victor <victor.basset@yahoo.fr>
2018-11-07 08:47:27 +0100

    BigInteger Test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
+import java.math.BigInteger;
+
+
 public class Main {
+
+    public static void main(String[] arg) {
+
+        BigInteger zac = new BigInteger("3");
+        zac = zac.pow(399);
+
+        BigInteger kon = new BigInteger("3");
+        kon = kon.pow(400);
+
+        BigInteger nul = new BigInteger("0");
+        BigInteger jed = new BigInteger("1");
+        BigInteger detel = new BigInteger("3");
+
+
+        for (BigInteger a = zac; a.compareTo(kon) <= 0; a = a.add(jed)) {
+        }
+    }
 }
