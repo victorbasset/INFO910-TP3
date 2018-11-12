4ef859ab3f02d8942d0f113e38f19cf46b97d947
Vincent PEILLEX <peillex.vincent@live.fr>
2018-11-12 18:34:51 +0100

    Programme <dechiffre> terminé

--- src/Chiffre.java
+++ src/Chiffre.java
@@ -3,6 +3,7 @@ import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,7 +13,7 @@ import java.util.Scanner;
 public class Chiffre {
     public static void main(String[] arg) {
 
-        // Lecture de l'entrée standard
+        // Lecture du message claire sur l'entrée standard
         final Scanner scan = new Scanner(System.in);
         final StringBuilder message = new StringBuilder();
         while (scan.hasNextLine()) {
@@ -20,26 +21,28 @@ public class Chiffre {
             if (scan.hasNextLine()) {
                 message.append('\n');
             }
-
         }
-//      System.out.println(message);
 
+        // Il faut obligatoirement spécifier un fichier
         if (arg.length != 1) {
             System.out.println("Usage : filename");
             return;
         }
 
+        // TODO : only use public file
         final String privateFilename = arg[0] + ".priv";
         final String publicFilename = arg[0] + ".pub";
 
+        // Lecture du fichier spécifié
         List<String> lines;
         try {
              lines = Files.readAllLines(Paths.get(privateFilename));
         } catch (IOException e) {
-            System.err.println("Impossible de lire le fichier contenant la clé privée");
+            System.err.println("Impossible de lire le fichier spécifié");
             return;
         }
 
+        // Lecture de la clé dans le fichier
         String privateKey = null;
         for (String line : lines) {
             if (line.contains("#")) {
@@ -52,6 +55,8 @@ public class Chiffre {
             final String[] params = privateKey.split(" ");
 
             if (params.length == 6) {
+
+                // On parse la clé en variables
                 final int t = Integer.parseInt(params[0]);
                 final BigInteger n = new BigInteger(params[1]);
                 final BigInteger b = new BigInteger(params[2]);
@@ -59,14 +64,15 @@ public class Chiffre {
                 final BigInteger q = new BigInteger(params[4]);
                 final BigInteger a = new BigInteger(params[5]);
 
-                // parcours du messag octet par octet
-               final List<BigInteger> splitedMessage = splitMessage(message.toString().getBytes(), t);
-               final List<BigInteger> cryptedBlocks = new ArrayList<>();
+                final List<BigInteger> splitedMessage = splitMessage(message.toString().getBytes(), t);
+                final List<BigInteger> cryptedBlocks = new ArrayList<>();
 
-               for (BigInteger block : splitedMessage) {
+                // crypte chaque bloc de taille t
+                for (BigInteger block : splitedMessage) {
                    cryptedBlocks.add(block.modPow(b, n));
-               }
+                }
 
+                // affiche les blocs sur la sortie standard
                 for (BigInteger block : cryptedBlocks) {
                     System.out.println(block);
                 }
@@ -81,27 +87,27 @@ public class Chiffre {
         }
     }
 
+    /**
+     * Découpe un message en block de taille <blockSize>
+     *
+     * @param message byte[]
+     * @param blockSize int
+     * @return List of BigInteger
+     */
     private static List<BigInteger> splitMessage(byte[] message, int blockSize) {
         final List<BigInteger> splitedMessage = new ArrayList<>();
-        int nbMessageBits = message.length * 8;
+
+        final int nbMessageBits = message.length * 8;
         int nbBlocks = nbMessageBits / blockSize;
         if (nbMessageBits % blockSize != 0) {
             nbBlocks++;
         }
 
         for (int i=0; i<nbBlocks; i++) {
-            final StringBuilder temp = new StringBuilder();
-            final int cursor = i * blockSize/8;
-
-            for (int j=cursor; j < cursor + blockSize/8 && j < message.length; j++) {
-                temp.append(message[j]);
-            }
-
-            if (temp.length() > 0) {
-                final BigInteger block = new BigInteger(temp.toString());
-                splitedMessage.add(block);
-            }
+            final byte[] slice = Arrays.copyOfRange(message, i * blockSize/8, i * blockSize/8 + blockSize/8);
+            splitedMessage.add(new BigInteger(slice));
         }
+
         return splitedMessage;
     }
 }
